13beb674207f4d56a8b50aaea16d38e3270931ca
Gina B Molina G <137374097+ginaGBMG@users.noreply.github.com>
2023-07-26 16:43:30 -0600

    Se agregaron ejercicios

---
+++ CommissionCalculator.java
@@ -0,0 +1,25 @@
+// Part 1: Commission Calculator
+public class CommissionCalculator {
+	//Part 1: Commission Calculator
+		import java.util.Scanner;
+			public static void main(String[] args) {
+			Scanner sc = new Scanner(System.in);
+			System.out.print("Comisión por venta:");
+			
+			for (int i=1 ; i<=N; i++) {
+				String monthStrings="";
+				case 1:
+					monthStrings="Enero";
+					break;
+				case 2:
+					monthStrings="Febrero";
+					break;
+				case 3:
+					monthStrings="Marzo";
+					break;
+			
+				}//
+				System.out.print();
+				in.close();
+					}//main
+				}//
